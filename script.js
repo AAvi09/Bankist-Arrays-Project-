07ec29ad02f50c40cf71b12a0e1e458827f48111
Avinash sharma <sharmaavi2196@gmail.com>
2025-03-10 22:41:03 +0530

    Add functionality to close account and remove user from accounts list

--- script.js
+++ script.js
@@ -156,6 +156,20 @@ btnTransfer.addEventListener('click', e => {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener('click', e => {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
